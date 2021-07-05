4470af50b08778e7fa6c94859fbf67bf2b60efb0
Nocxy <36235075+Nocxy@users.noreply.github.com>
2021-07-05 22:26:53 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,17 +6,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	  Scanner scanner = new Scanner(System.in);
-	  int game_size = scanner.nextInt();
-	  int seed = scanner.nextInt();
-	  int number_of_generations = scanner.nextInt();
-	  MatrixGeneration this_matrix = new MatrixGeneration(game_size, seed, number_of_generations);
-	  this_matrix.seed = seed;
+	Scanner scanner = new Scanner(System.in);
+	int game_size = scanner.nextInt();
+	int seed = scanner.nextInt();
+	int number_of_generations = scanner.nextInt();
+	char[][] current_gen;
+	MatrixGeneration this_matrix = new MatrixGeneration(game_size, seed, number_of_generations);
+	current_gen = this_matrix.current_gen;
+	this_matrix.seed = seed;
 
-	  for (int i = 0 ; i < number_of_generations ; i++){
-		    char[][] current_gen = this_matrix.Generate_next(game_size);
-	  }
+	for (int i = 0 ; i < number_of_generations ; i++){
+		current_gen = this_matrix.Generate_next(game_size);
+	}
 
-	  this_matrix.DrawBoard(current_gen[][]);
+	this_matrix.DrawBoard(current_gen);
     }
 }
